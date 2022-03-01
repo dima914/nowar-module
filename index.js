503e8a05a4df195d3f857a5ed9d63e71c9ec1e95
vdawork <vdawork@gmail.com>
2022-03-01 20:50:55 +0200

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.getPicture = function() {
+    return "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIF.41VNbNl1qA%252ftk22cY5R32w%26pid%3DApi&f=1";
+}
